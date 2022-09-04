395a73fd606a7c4807db29f1ca9a1388b6d58ac5
connor <connorjbaba@gmail.com>
2022-09-04 11:27:44 -0400

    create Die component. need to find why it isn't rendering

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 
+import Die from './Die'
 
 
 function App() {
@@ -7,7 +8,18 @@ function App() {
 
   return (
     <div className="App">
-      <p>test</p>
+      <div className="main-container">
+        <div className="text-container">
+        <h1 className="title">Tenzi</h1>
+        <p>Roll until all dice are the same. Click each die to freeze it at it's current value between rolls.</p>
+        </div>
+        <div className="die-container">
+          <Die />
+          <Die />
+
+        </div>
+        <div className="button-container"></div>
+      </div>
     </div>
   )
 }
---
+++ src/Die.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+export default function Die() {
+
+    <div className="test">
+        <h1>test</h1>
+    </div>
+}
